f573fe8b14207bac90ad2805e0b6e4d2498e562d
cluck-mur <cluck.mur@ahk.jp>
2020-10-02 02:05:31 +0900

    "レッスン5. 動的ルーティング" - "投稿ページに title を追加する" まで進捗した

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,14 +1,22 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
 
 export default function Post({ postData }) {
-    return <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-    </Layout>
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+        </Layout>
+    )
 }
 
 export async function getStaticPaths() {
@@ -24,7 +32,7 @@ export async function getStaticPaths() {
  * @param {*} param0 
  */
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
         postData
