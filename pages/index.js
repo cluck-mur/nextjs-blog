65334699432364cb8f34c605d16edabf45a812e6
cluck-mur <cluck.mur@ahk.jp>
2020-10-05 19:31:03 +0900

    "レッスン6." - "動的 API ルート" まで進捗した。

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,22 @@ export default function Home({allPostData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>こんにちは！かつつーです！<br />
-        まだまだこれからやけんね！</p>
+        <p>
+          {/*
+          Prepare for trouble,<br />
+          and make it double<br />
+          To protect the world from devastation,<br />
+          To unite all people within our nation,<br />
+          To denounce the evil of truth and love,<br />
+          To extend our reach to the stars above<br />
+          Jesse!<br />
+          James!<br />
+          Team Rocket blasts off at the speed of light,<br />
+          Surrender now, or prepare to fight<br />
+          Meowth, that's right!<br />
+          */}
+          Hello there! Welcome to the world of Pokémon! My name is Oak! People call me the Pokémon Prof! This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession. First, what is your name? Right! So your name is [player]! This is my grandson. He's been your rival since you were a baby. …Erm, what is his name again? That's right! I remember now! His name is [rival]! [player]! Your very own Pokémon legend is about to unfold! A world of dreams and adventures with Pokémon awaits! Let's go!
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
